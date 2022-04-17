fa798028d26b57da744aa0c2b56b80bf5d96aa3f
Jarsky <jarskynz@gmail.com>
2022-04-17 15:44:28 +1200

    Update qBitTorrent-Cleanup.sh

--- qBitTorrent-Cleanup.sh
+++ qBitTorrent-Cleanup.sh
@@ -7,26 +7,30 @@ qBitTorrentLogPath=/opt/appdata/qbittorrent/config/qBittorrent/logs
 LogPath=/var/log/qBitTorrent-Cleanup.log
 
 #Script
-dateFormat="%Y-%m-%dT%H:%M:%S"
+ dateFormat="%Y-%m-%dT%H:%M:%S"
 exec 1>> >(ts '['$dateFormat']' >> "$LogPath") 2>&1
-files=`sudo cat $qBitTorrentLogPath/qbittorrent.log | grep "Error: Directory not empty" | awk '{ print $4 }' | tr -s "\'" ' '`
-exists=`sudo cat $LogPath | grep "will be deleted" | awk '{ print $2 }' | sed -r 's:^'$torrentPath'/::' | sed 's/.$//'`
+files=`cat $qBitTorrentLogPath/qbittorrent.log | grep "Error: Directory not empty" | awk '{ print $4 }' | tr -s "\'" ' '`
+exists=`cat $LogPath | grep "will be deleted" | awk '{ print $3 }' | sed -r 's:^'$torrentPath'/::' | sed 's/.$//'`
 array=($files)
 value=($exists)
 
-#if [[ " ${array[*]} " =~ " ${value} " ]]; then
-#    for i in "${array[@]}"
-#        do
-#                echo "$torrentPath/$i/ was already deleted"
-#        done
-#fi
-
-if [[ ! " ${array[*]} " =~ " ${value} " ]]; then
+if [[ " ${array[*]} " =~ " ${value} " ]]; then
     for i in "${array[@]}"
         do
-                echo "$torrentPath/$i/ will be deleted"
+                echo "[INFO] $torrentPath/$i/ was already deleted"
+        done
+fi
+
+if [[ ! " ${array[*]} " =~ " ${value} " ]]; then
+
+    for i in "${array[@]}"
+        do
                 if [ $deleteFiles = "true" ]; then
+                echo "[INFO] $torrentPath/$i/ will be deleted"
                 rm -rf $torrentPath/$i/
+                else
+                echo "[WARN] $torrentPath/$i/ will be deleted (TEST)"
                 fi
         done
 fi
+
