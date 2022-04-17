c119be859abf9ff320198fed490543dcd913076b
Jarsky <jarskynz@gmail.com>
2022-04-17 15:24:10 +1200

    qBitTorrent-Cleanup.sh

--- qBitTorrent-Cleanup.sh
+++ qBitTorrent-Cleanup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #Config
-deleteFiles="true"
+deleteFiles="false"
 torrentPath=/path/to/my/files
 qBitTorrentLogPath=/opt/appdata/qbittorrent/config/qBittorrent/logs
 LogPath=/var/log/qBitTorrent-Cleanup.log
 
 #Script
- dateFormat="%Y-%m-%dT%H:%M:%S"
+dateFormat="%Y-%m-%dT%H:%M:%S"
 exec 1>> >(ts '['$dateFormat']' >> "$LogPath") 2>&1
 files=`sudo cat $qBitTorrentLogPath/qbittorrent.log | grep "Error: Directory not empty" | awk '{ print $4 }' | tr -s "\'" ' '`
 exists=`sudo cat $LogPath | grep "will be deleted" | awk '{ print $2 }' | sed -r 's:^'$torrentPath'/::' | sed 's/.$//'`
