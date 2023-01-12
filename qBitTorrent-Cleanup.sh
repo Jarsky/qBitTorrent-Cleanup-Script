c35fc6fd8219cd6bb00da6c0d54169774469dacd
Jarrod <jarskynz@gmail.com>
2023-01-12 19:18:31 +1300

    Update qBitTorrent-Cleanup.sh

--- qBitTorrent-Cleanup.sh
+++ qBitTorrent-Cleanup.sh
@@ -14,25 +14,24 @@
 #
 # Config
 deleteFiles="false"
-torrentPath="/path/to/downloads"
-qBitTorrentLogPath="/opt/qbittorrent/config/qBittorrent/logs"
-logPath="/var/log"
-qBTlog="qbittorrent.log"
-qBTClean="qBitTorrent-Cleanup.log"
-
-# Script
+torrentPath=/path/to/downloads
+qBitTorrentLogPath=/opt/qbittorrent/config/qBittorrent/logs
+logPath=/var/log
+qBTlog=qbittorrent.log
+qBTClean=qBitTorrent-Cleanup.log
 dateFormat=$(date -u +"[%Y-%m-%dT%H:%M:%S]")
 
+# Check for log files
 if [ ! -f "$qBitTorrentLogPath/$qBTlog" ]; then
-  echo "$dateFormat [WARN] Cannot find $qBTlog. Check the path to your qBitTorrent Logs."
+  echo "[WARN] Cannot find $qBTlog. Check the path to your qBitTorrent Logs."
   exit 1
 elif [ ! -r "$qBitTorrentLogPath/$qBTlog" ]; then
-  echo "$dateFormat [WARN] Cannot read from $qBTlog. Check permissions."
+  echo "[WARN] Cannot read from $qBTlog. Check permissions."
   exit 1
 elif [ ! -f "$logPath/$qBTClean" ]; then
   touch "$logPath/$qBTClean"
 elif [ ! -w "$logPath/$qBTClean" ]; then
-  echo "$dateFormat [WARN] Cannot write to $logPath/$qBTClean . Check permissions."
+  echo "[WARN] Cannot write to $logPath/$qBTClean . Check permissions."
   exit 1
 fi
 
@@ -44,10 +43,25 @@ while IFS= read -r folder; do
   if grep -q "$folder" "$logPath/$qBTClean"; then
     continue
   else
-    if [ "$deleteFiles" = "true" ]; then
-      rm -rf "$torrentPath/$folder"
-      echo "$dateFormat [INFO] Deleted $folder" >> "$logPath/$qBTClean"
+    if [ $deleteFiles = "true" ]; then
+      if [ -d "$torrentPath/$folder" ]; then
+        echo "$dateFormat [INFO] Deleting $torrentPath/$folder"
+        rm -r "$torrentPath/$folder"
+        if [ $? -eq 0 ]; then
+          echo "$dateFormat [INFO] $folder deleted successfully."
+          echo "$dateFormat [FLCK] $folder" >> "$logPath/$qBTClean"
+        else
+          echo "$dateFormat [WARN] $folder not deleted. Possibly file locked."
+        fi
+      else
+        echo "$dateFormat [FLCK] $torrentPath/$folder does not exist."
+      fi
+    else
+      if [ -d "$torrentPath/$folder" ]; then
+        echo "$dateFormat [TEST] $folder exists but is INFO only mode"
+      else
+        echo "$dateFormat [TEST] $folder does not exist in: $torrentPath"
+      fi
     fi
-    echo "$dateFormat [TEST] Deleted $folder" >> "$logPath/$qBTClean"
   fi
 done <<< "$folders"
